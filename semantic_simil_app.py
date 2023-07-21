6c753c1e5175ed4780696ba6306415ccd34b3fa3
Shilpa Ajith <133002262+ShilpaAjitheks@users.noreply.github.com>
2023-07-21 23:22:15 +0530

    Update semantic_simil_app.py
    Parameter to avoid warning in cache added

--- semantic_simil_app.py
+++ semantic_simil_app.py
@@ -52,7 +52,7 @@ def spacy_lemmatizer(text):
 
 # Load the saved model
 #Using st.cache to load model only once
-@st.cache
+@st.cache(ignore_hash=True)
 def load_model():
 	  return SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 
